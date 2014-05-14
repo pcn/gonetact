a10cf3750f95e3f46180fb97594fa50d0e371a48
Peter C. Norton <peter@statelessnetworks.com>
2014-05-14 10:13:56 -0400

    Make use of the local browser selectable.
    - Use a struct to hold configuration/options
    - Put command line parsing into its own function
    - Add a flag to disable opening the auth URL in a browser

--- gonetact.go
+++ gonetact.go
@@ -31,23 +31,33 @@ import (
 	"github.com/docopt/docopt-go"
 )
 
-func main() {
+type CmdLineOpts struct {
+	client_id  string // filename
+	cache_file string // filename
+	username   string // gmail email address
+	query	   string // substring to match
+	no_browser bool   // don't open the browser
+}
+
+func readCommandLine() *CmdLineOpts {
 	usr, err := user.Current()
+	rc_dir := fmt.Sprintf("%s/.gonetact", usr.HomeDir)
 	if err != nil {
 		log.Fatal( err )
 	}
+	opts := new(CmdLineOpts)
 
-	rc_dir := fmt.Sprintf("%s/.gonetact", usr.HomeDir)
 	docstring := fmt.Sprintf(`Limited interaction with google contacts
 
 Usage:
-  gonetact [-o] [--client-id=<filename>] [--cache=<cache-file>] [--query=<query>]
+  gonetact [--client-id=<filename>] [--cache=<cache-file>] [--query=<query>] [--no-browser]
 
 Options:
   --client-id=<filename>    file containing a json client_id [default: %[1]s/client.json]
   --cache=<filename>        file to cache the access token[default: %[1]s/cache.json]
   --user=<gmail address>    user whose contacts will be authenticated
   --query=<query>           match this string in the email and name of the contact
+  --no-browser              Don't open the auth link a browser [default: false]
   -h --help                 Show this message
 
 The client_id is a file containing a json document per
@@ -55,12 +65,27 @@ https://code.google.com/apis/console#access`, rc_dir)
 
 	args, _ := docopt.Parse(docstring, nil, true, "goneact 0.1", false)
 
-	// fmt.Println(args)
-	transport := get_oauth_token(string(args["--client-id"].(string)), string(args["--cache"].(string)))
+	opts.client_id  = string(args["--client-id"].(string))
+	opts.no_browser = bool(args["--no-browser"].(bool))
+	opts.cache_file = string(args["--cache"].(string))
+	if args["--user"] != nil {
+		opts.username   = string(args["--user"].(string))
+	}
+	if args["--query"] != nil {
+		opts.query      = string(args["--query"].(string))
+	}
+	return opts
+}
+
+func main() {
+	opts := readCommandLine()
+
+	transport := get_oauth_token(opts.client_id, opts.cache_file, opts.no_browser)
+
 	// fmt.Println(transport)
-	if args["--query"] == nil {
+	if opts.query == "" {
 		print_all_contacts(transport)
 	} else {
-		print_matching_contacts(transport, args["--query"].(string))
+		print_matching_contacts(transport, opts.query)
 	}
 }
