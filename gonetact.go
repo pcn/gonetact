f67da439b53eefc04ad61e5f83e9d2c2e773ce3e
Peter C. Norton <peter@statelessnetworks.com>
2014-05-14 11:42:10 -0400

    Tests for command-line parsing
    - Removed the user option.  It's not used at the moment.

--- gonetact.go
+++ gonetact.go
@@ -5,6 +5,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 	"os/user"
 	"github.com/docopt/docopt-go"
 )
@@ -17,7 +18,7 @@ type CmdLineOpts struct {
 	no_browser bool   // don't open the browser
 }
 
-func readCommandLine() *CmdLineOpts {
+func readCommandLine(argv []string) *CmdLineOpts {
 	usr, err := user.Current()
 	rc_dir := fmt.Sprintf("%s/.gonetact", usr.HomeDir)
 	if err != nil {
@@ -33,7 +34,6 @@ Usage:
 Options:
   --client-id=<filename>    file containing a json client_id [default: %[1]s/client.json]
   --cache=<filename>        file to cache the access token[default: %[1]s/cache.json]
-  --user=<gmail address>    user whose contacts will be authenticated
   --query=<query>           match this string in the email and name of the contact
   --no-browser              Don't open the auth link a browser [default: false]
   -h --help                 Show this message
@@ -41,14 +41,11 @@ Options:
 The client_id is a file containing a json document per
 https://code.google.com/apis/console#access`, rc_dir)
 
-	args, _ := docopt.Parse(docstring, nil, true, "goneact 0.1", false)
+	args, _ := docopt.Parse(docstring, argv[1:], true, "goneact 0.1", false)
 
 	opts.client_id  = string(args["--client-id"].(string))
-	opts.no_browser = bool(args["--no-browser"].(bool))
 	opts.cache_file = string(args["--cache"].(string))
-	if args["--user"] != nil {
-		opts.username   = string(args["--user"].(string))
-	}
+	opts.no_browser = bool(args["--no-browser"].(bool))
 	if args["--query"] != nil {
 		opts.query      = string(args["--query"].(string))
 	}
@@ -56,7 +53,7 @@ https://code.google.com/apis/console#access`, rc_dir)
 }
 
 func main() {
-	opts := readCommandLine()
+	opts := readCommandLine(os.Args)
 
 	transport := get_oauth_token(opts.client_id, opts.cache_file, opts.no_browser)
 
